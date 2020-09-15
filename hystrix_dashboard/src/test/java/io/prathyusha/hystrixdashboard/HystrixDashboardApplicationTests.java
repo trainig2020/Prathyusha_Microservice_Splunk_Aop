9adf2aea457e84650161786f2b7aa7c9a71f3af8
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-09-15 21:53:06 +0530

    Add files via upload

---
+++ hystrix_dashboard/src/test/java/io/prathyusha/hystrixdashboard/HystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.prathyusha.hystrixdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
